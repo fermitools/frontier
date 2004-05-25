52880d27a80b48af6b7223bce2f0339b67f7d077
swhite <swhite>
2004-05-25 16:07:14 +0000

    Added an structure for tests.

---
+++ test/gov/fnal/CommandTest.java
@@ -0,0 +1,28 @@
+import junit.framework.TestCase;
+
+public class CommandTest extends TestCase {
+    
+    public CommandTest(String name) {
+	super(name);
+    }
+
+    public void setUp() {
+	// Do nutton for now
+    }
+
+    public void tearDown() {
+	// Do nutton for now
+    }
+
+    public void testSetUniversalQueryRequest() {
+	// do nutton for now
+	long x = 5/0;
+    }
+
+    public void testSetAdminRequest() {
+	// do nutton for now
+    }
+
+
+    
+}
