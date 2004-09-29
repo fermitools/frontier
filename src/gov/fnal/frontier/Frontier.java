004cf6587051bd4911af4b3857cb0d512b53dfc7
swhite <swhite>
2004-09-29 17:09:09 +0000

    Prints out the message and stack associated with bad quality sections.

--- src/gov/fnal/frontier/Frontier.java
+++ src/gov/fnal/frontier/Frontier.java
@@ -39,7 +39,7 @@ public final class Frontier extends CacheHttpServlet {
                       HttpServletResponse response) throws IOException, ServletException {
 
         int local_current;
-        int id;
+        Identifier id;
         Calendar timestamp = Calendar.getInstance();
         DbConnectionMgr connMgr = null;
         CommandParser parser = null;
@@ -47,84 +47,94 @@ public final class Frontier extends CacheHttpServlet {
         ServletOutputStream writer = null;
 
         synchronized (mutex) {
-            id = count_total;
             ++count_total;
             ++count_current;
             local_current = count_current;
+            id = new Identifier(count_total);
         }
 
-        String queryString = URLDecoder.decode(request.getQueryString(),"UTF-8");
-        System.out.println("frontierLog " + timestamp.getTime() + " start " + id + " "
-                           + local_current + " " + queryString);
-
-        response.setContentType("text/xml");
-        response.setCharacterEncoding("US-ASCII");
-        // For Squid
-        response.setDateHeader("Expires", timestamp.getTimeInMillis() + time_expire);
-
-        connMgr = DbConnectionMgr.getDbConnectionMgr();
-        parser = new CommandParser();
-        writer = response.getOutputStream();
-
-        writer.println("<?xml version=\"1.0\" encoding=\"US-ASCII\"?>");
-        writer.println("<frontier version=\"" + frontierVersion + "\" xmlversion=\"" + xmlVersion
-                       + "\">");
         try {
-            commandList = parser.parse(queryString);
-            connMgr.initialize();
-            handleRequest(commandList, writer);
-        } catch (CommandParserException e) {
-            writer.println("<transaction payloads=\"0\"/>");
-            writer.println(generateBadQualityTag(e.getMessage()));
-        } catch (DbConnectionMgrException e) {
-            /** @todo Need to cleaup th /transaction clauses */
-            writer.println("</transaction>");
-            System.out.println("Unable to obtain connection: " + e.getMessage());
-            writer.println(generateBadQualityTag(e.getMessage()));
-        } catch (Exception e) {
-            writer.println("</transaction>");
-            System.out.println("Error: " + e);
-            e.printStackTrace();
-            writer.println(generateBadQualityTag(e.getMessage()));
+            String queryString = URLDecoder.decode(request.getQueryString(), "UTF-8");
+            System.out.println("<frontierLog " + timestamp.getTime()
+                               + " ID: " + id.getIdentifier() + ">  "
+                               + " start "
+                               + " threads: " + local_current
+                               + " queryString: " + queryString);
+
+            response.setContentType("text/xml");
+            response.setCharacterEncoding("US-ASCII");
+            // For Squid
+            response.setDateHeader("Expires", timestamp.getTimeInMillis() + time_expire);
+
+            connMgr = DbConnectionMgr.getDbConnectionMgr();
+            parser = new CommandParser();
+            writer = response.getOutputStream();
+
+            writer.println("<?xml version=\"1.0\" encoding=\"US-ASCII\"?>");
+            writer.println("<frontier version=\"" + frontierVersion + "\" xmlversion=\"" +
+                           xmlVersion
+                           + "\">");
+            try {
+                commandList = parser.parse(queryString);
+                connMgr.initialize();
+                handleRequest(id,commandList, writer);
+            } catch (CommandParserException e) {
+                writer.println("<transaction payloads=\"0\"/>");
+                writer.println(generateBadQualityTag(id,e));
+            } catch (DbConnectionMgrException e) {
+                /** @todo Need to cleaup th /transaction clauses in this class*/
+                writer.println("</transaction>");
+                System.out.println("Unable to obtain connection: " + e.getMessage());
+                writer.println(generateBadQualityTag(id,e));
+            } catch (Exception e) {
+                writer.println("</transaction>");
+                System.out.println("Error: " + e);
+                e.printStackTrace();
+                writer.println(generateBadQualityTag(id,e));
+            }
+
+        } finally {
+            writer.println("</frontier>");
+            writer.close();
+            synchronized (mutex) {
+                --count_current;
+                local_current = count_current;
+            }
+
+            Calendar timestamp2 = Calendar.getInstance();
+            System.out.println("<frontierLog " + timestamp2.getTime()
+                               + " ID: " + id.getIdentifier() + ">  "
+                               + " stop "
+                               + " threads: " + local_current + " elapsedTime: "
+                               + (timestamp2.getTimeInMillis() - timestamp.getTimeInMillis()));
         }
-        writer.println("</frontier>");
-
-        writer.close();
-
-        synchronized (mutex) {
-            --count_current;
-            local_current = count_current;
-        }
-
-        Calendar timestamp2 = Calendar.getInstance();
-        System.out.println("frontierLog " + timestamp2.getTime() + " stop  " + id + " "
-                           + local_current + " "
-                           + (timestamp2.getTimeInMillis() - timestamp.getTimeInMillis()));
     }
 
     /**
      * Oversees the processing of Commands by distributing  each command to the
      * appropriate handler.
+     * @param Identifier object which shows who called the method.
      * @param commandList An ArrayList of Command objects which are to be handled.
      * @param writer      The output writer.println to respond to the Command on.
      * @throws ServletException if a servlet error occurs
      */
-    private void handleRequest(ArrayList commandList, ServletOutputStream writer) throws Exception {
+    private void handleRequest(Identifier id, ArrayList commandList, ServletOutputStream writer)
+        throws Exception {
         RequestHandler handler = null;
         int numCommands = commandList.size();
         writer.println("<transaction payloads=\"" + numCommands + "\">");
         for (int i = 0;i < numCommands;i++) {
             Command command = (Command) commandList.get(i);
             if (command.isUniversalQueryCommand())
-                handler = new UniversalQueryRequestHandler(writer);
+                handler = new UniversalQueryRequestHandler(id, writer);
             else if (command.isAdminCommand())
-                handler = new AdministrationRequestHandler(writer);
+                handler = new AdministrationRequestHandler(id, writer);
             else
                 throw new ServletException("Internal error, unknown type of Command.");
             try {
                 handler.process(command);
             } catch (RequestHandlerException e) {
-                generateBadQualityTag(e.getMessage());
+                generateBadQualityTag(id,e);
             }
         }
         writer.println("</transaction>");
@@ -132,11 +142,26 @@ public final class Frontier extends CacheHttpServlet {
 
     /**
      * Outputs an XML tag to the writer.println indicating an error condtion
+     * @param id {@link Identifier}
      * @param message Text message to place into writer.println
      * @throws ServletException if a servlet error occurs
      */
-    private String generateBadQualityTag(String message) {
-        return "<quality error=\"1\" code=\"???\" message=\"" + message + "\"/>";
+    private String generateBadQualityTag(Identifier id, Exception e) {
+        recordError(id,e);
+        return "<quality error=\"1\" message=\"" + e.getMessage() + "\"/>";
     }
+
+    /**
+     * Outputs the message from the quality error to the log file.
+     * @param message String text contining the error message.
+     * @todo Need a debug class to handle such messages.
+     */
+    private void recordError(Identifier id, Exception e) {
+        Calendar timestamp = Calendar.getInstance();
+        System.out.println("<frontierLog " + timestamp.getTime()
+                           + "ID: " + id.getIdentifier() + "> error: " + e.getMessage());
+        e.printStackTrace();
+    }
+
 
 } // class Frontier
