eb6186508aa855b6b4e983e66b3af4f5177f82ac
serge <serge>
2005-02-17 20:15:40 +0000

    *** empty log message ***

--- src/gov/fnal/frontier/fdo/FrontierDataObject.java
+++ src/gov/fnal/frontier/fdo/FrontierDataObject.java
@@ -1,8 +1,18 @@
-package gov.fnal.frontier;
+package gov.fnal.frontier.fdo;
 
 import java.io.*;
 import gov.fnal.frontier.codec.Encoder;
 
+/**
+ * Interface to Frontier Data Object. Any DataObject implementation
+ * must implement this interface
+ *
+ * @author Server Kosyakov
+ * @version $Id$
+ * @see gov.fnal.frontier.XsdDataObject
+ * @see gov.fnal.frontier.Xsd2DataObject
+ * @see gov.fnal.frontier.PluginDataObject
+ */
 public interface FrontierDataObject
  {
   public void fdo_init(byte[] body) throws Exception;
