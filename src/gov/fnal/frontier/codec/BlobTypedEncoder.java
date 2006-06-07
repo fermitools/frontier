16c20d553bb7f56895f3cb2a81557cf84318f02d
dwd <dwd>
2006-06-07 18:11:56 +0000

    Add support for zipping in the BLOB encoding.  Expects a new second parameter to the constructor which optionally contains "zip" to zip optionally followed by a digit 0 to 9 for the zip level. Also added a debug outputstream "MyDebugOutputStream", currently unused, which prints the contents of an output stream to System.out.  It can be inserted anywhere into the pipeline of output streams.

--- src/gov/fnal/frontier/codec/BlobTypedEncoder.java
+++ src/gov/fnal/frontier/codec/BlobTypedEncoder.java
@@ -15,8 +15,37 @@ package gov.fnal.frontier.codec;
 
 import java.io.*;
 import java.security.*;
+import java.util.zip.*;
 import gov.fnal.frontier.fdo.Encoder;
 
+/* this filter can be inserted to print out a datastream in hex */
+class MyDebugOutputStream extends java.io.FilterOutputStream
+ {
+  private int cnt=0;
+  public MyDebugOutputStream(OutputStream out)
+   {
+    super(out);
+   }
+
+  public void write(int b) throws IOException
+   {
+    if (cnt++ == 0) System.out.print("MyDebugOut: [");
+    String hs = Integer.toHexString(b&0xff);
+    if (hs.length() == 1) hs="0"+hs;
+    System.out.println(hs);
+    super.write(b);
+   }
+
+  public void flush() throws IOException
+   {
+    if (cnt > 0)
+     {
+      System.out.println("] "+cnt+" bytes written");
+      cnt = 0;
+     }
+    super.flush();
+   }
+ }
 
 public class BlobTypedEncoder implements Encoder
  {
@@ -37,17 +66,57 @@ public class BlobTypedEncoder implements Encoder
   private Base64.OutputStream b64os;
   private ByteArrayOutputStream baos;
   private OutputStream channel;
+  private DigestOutputStream dgos;
+  private DeflaterOutputStream dfos;
 
+  private int ziplevel=0;
   private long out_size=0;
 
-  public BlobTypedEncoder(OutputStream out) throws Exception
+  public BlobTypedEncoder(OutputStream out,String param) throws Exception
    {
     channel=out;
 
     baos=new ByteArrayOutputStream();
     b64os=new Base64.OutputStream(baos);
     md5=MessageDigest.getInstance("MD5");
-    os=new DataOutputStream(new DigestOutputStream(b64os,md5));
+    dgos=new DigestOutputStream(b64os,md5);
+
+    if ((param.length() >= 3) && (param.substring(0,3).equals("zip")))
+     {
+      String zipparam=param.substring(3);
+      if (zipparam.equals(""))
+       {
+	ziplevel=5;
+       }
+      else
+       {
+	Integer zipint;
+	try {zipint=new Integer(zipparam);}
+	catch(Exception e)
+	 {
+	  throw new Exception("BLOB zip level "+zipparam+" not an integer");
+	 }
+        ziplevel=zipint.intValue();
+        if ((ziplevel < 0) || (ziplevel > 9))
+	 {
+          throw new Exception("BLOB zip level less than 0 or greater than 9");
+	 }
+       }
+     }
+    else if (param.length() != 0)
+     throw new Exception("Unrecognized BLOB sub-encoding "+param);
+    System.out.println("BLOB zip level ["+ziplevel+"]");
+
+    if (ziplevel > 0)
+     {
+      dfos=new DeflaterOutputStream(dgos,new Deflater(ziplevel));
+      os=new DataOutputStream(dfos);
+     }
+    else
+     {
+      dfos=null;
+      os=new DataOutputStream(dgos);
+     }
    }
 
 
@@ -166,6 +235,7 @@ public class BlobTypedEncoder implements Encoder
   public void flush() throws Exception
    {
     os.flush();
+    if (dfos!=null) dfos.finish();
     b64os.flushBase64();
     dump();
    }
@@ -176,6 +246,7 @@ public class BlobTypedEncoder implements Encoder
     flush();
     os.close();
     os=null;
+    dfos=null;
     b64os=null;
    }
  }
