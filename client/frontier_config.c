c78d323caa33e19eae5758600605876ffc06ae07
dwd <dwd>
2006-07-11 17:46:48 +0000

    Add handling complex server connection strings containing parenthesized keyword-value pairs.  This was added to make it straightforward for higher CMS software levels to pass a single string to specify one connection.   The format is (keyword=value)...  where keyword is one of serverurl, proxyurl, or retrieve-ziplevel.  Keywords can appear multiple times, and anything between parenthesized components is ignored.  Also change frontierConfig_get to return an error code if something goes wrong.

--- client/frontier_config.c
+++ client/frontier_config.c
@@ -18,6 +18,9 @@
 #include "frontier_client/frontier_log.h"
 #include "frontier_client/frontier_error.h"
 
+// default retrieve zip level is the normal zip level 5
+static int default_retrieve_zip_level = 5;
+
 #define ENV_BUF_SIZE	1024
 
 void *(*frontier_mem_alloc)(size_t size);
@@ -34,36 +37,54 @@ static char *str_dup(const char *str)
  }
 
 
-FrontierConfig *frontierConfig_get(const char *server_url,const char *proxy_url)
+FrontierConfig *frontierConfig_get(const char *server_url,const char *proxy_url,int *errorCode)
  {
   FrontierConfig *cfg;
   int i;
   char buf[ENV_BUF_SIZE];
   
   cfg=(FrontierConfig*)frontier_mem_alloc(sizeof(FrontierConfig));
-  if(!cfg) return cfg;
+  if(!cfg)
+   {
+    *errorCode = FRONTIER_EMEM;
+    return 0;
+   }
   bzero(cfg,sizeof(FrontierConfig));
+
+  cfg->retrieve_zip_level = default_retrieve_zip_level;
   
   // First add configured server and proxy.
-  frontierConfig_addServer(cfg, server_url);
-  frontierConfig_addProxy(cfg, proxy_url);
+  *errorCode = frontierConfig_addServer(cfg, server_url);
+  if (*errorCode != FRONTIER_OK) goto cleanup;
+  *errorCode = frontierConfig_addProxy(cfg, proxy_url);
+  if (*errorCode != FRONTIER_OK) goto cleanup;
  
   // Add additional servers/proxies from env variables.
-  frontierConfig_addServer(cfg, getenv(FRONTIER_ENV_SERVER));
-  frontierConfig_addProxy(cfg, getenv(FRONTIER_ENV_PROXY));
+  *errorCode = frontierConfig_addServer(cfg, getenv(FRONTIER_ENV_SERVER));
+  if (*errorCode != FRONTIER_OK) goto cleanup;
+  *errorCode = frontierConfig_addProxy(cfg, getenv(FRONTIER_ENV_PROXY));
+  if (*errorCode != FRONTIER_OK) goto cleanup;
   for(i = 0; i < FRONTIER_MAX_SERVERN; i++) {
     snprintf(buf, ENV_BUF_SIZE, "%s%d", FRONTIER_ENV_SERVER, (i+1));
-    frontierConfig_addServer(cfg, getenv(buf));
+    *errorCode = frontierConfig_addServer(cfg, getenv(buf));
+    if (*errorCode != FRONTIER_OK) goto cleanup;
   }
   frontier_log(FRONTIER_LOGLEVEL_DEBUG, __FILE__, __LINE__, "Total %d servers", cfg->server_num);
   
   for(i = 0; i < FRONTIER_MAX_PROXYN; i++) {
     snprintf(buf, ENV_BUF_SIZE, "%s%d", FRONTIER_ENV_PROXY, (i+1));
-    frontierConfig_addProxy(cfg, getenv(buf));
+    *errorCode = frontierConfig_addProxy(cfg, getenv(buf));
+    if (*errorCode != FRONTIER_OK) goto cleanup;
   }
   frontier_log(FRONTIER_LOGLEVEL_DEBUG, __FILE__, __LINE__, "Total %d proxies", cfg->proxy_num);     
     
+  frontier_log(FRONTIER_LOGLEVEL_DEBUG, __FILE__, __LINE__, "Retrieve zip level is %d", cfg->retrieve_zip_level);     
+
   return cfg;
+
+cleanup:
+  frontierConfig_delete(cfg);
+  return 0;
  }
 
 
@@ -116,21 +137,129 @@ void frontierConfig_delete(FrontierConfig *cfg)
   frontier_mem_free(cfg);
  }
 
+int frontierConfig_getRetrieveZipLevel(FrontierConfig *cfg)
+ {
+  return cfg->retrieve_zip_level;
+ }
+
+void frontierConfig_setRetrieveZipLevel(FrontierConfig *cfg,int level)
+ {
+  if (level < 0)
+    level = 0;
+  if (level > 9)
+    level = 9;
+  cfg->retrieve_zip_level = level;
+ }
+
+void frontierConfig_setDefaultRetrieveZipLevel(int level)
+ {
+  if (level < 0)
+    level = 0;
+  if (level > 9)
+    level = 9;
+  default_retrieve_zip_level = level;
+ }
+
+
+int frontierConfig_parseComplexServerSpec(FrontierConfig *cfg, const char* server_spec)
+ {
+  char *str = str_dup(server_spec);
+  char *p = str-1;
+  char *keyp, *valp;
+  int nestlevel = 0;
+  int ret;
+
+  frontier_log(FRONTIER_LOGLEVEL_DEBUG, __FILE__, __LINE__, "Parsing complex server spec %s",server_spec);    
+
+  if (str == NULL)
+    return FRONTIER_EMEM;
+   
+  while (*++p)
+   {
+    switch(*p)
+     {
+      case '(':
+	if (nestlevel == 0)
+	 {
+	  keyp = p+1;
+	  valp = 0;
+	 }
+        nestlevel++;
+        break;
+      case '=':
+        if (nestlevel != 1)
+	  continue;
+	*p = '\0';
+	valp = p+1;
+	break;
+      case ')':
+        --nestlevel;
+        if (nestlevel < 0)
+	 {
+	  frontier_setErrorMsg(__FILE__, __LINE__, "Too many close-parens in server spec");
+	  ret = FRONTIER_ECFG;
+	  goto cleanup;
+	 }
+	if (nestlevel > 0)
+	  continue;
+	*p = '\0';
+	if (valp == 0)
+	 {
+	  frontier_setErrorMsg(__FILE__, __LINE__, "No '=' after keyword %s",keyp);
+	  ret = FRONTIER_ECFG;
+	  goto cleanup;
+	 }
+	ret = FRONTIER_OK;
+	if (strcmp(keyp, "serverurl") == 0)
+	  ret = frontierConfig_addServer(cfg, valp);
+	else if (strcmp(keyp, "proxyurl") == 0)
+	  ret = frontierConfig_addProxy(cfg, valp);
+	else if (strcmp(keyp, "retrieve-ziplevel") == 0)
+	  frontierConfig_setRetrieveZipLevel(cfg, atoi(valp));
+	else
+	 {
+	 /* else ignore unrecognized keys */
+	  frontier_log(FRONTIER_LOGLEVEL_DEBUG, __FILE__, __LINE__, "Unrecognized keyword %s",keyp);
+	 }
+
+	if (ret != FRONTIER_OK)
+	  goto cleanup;
+	break;
+     }
+   }
+
+  if (nestlevel > 0)
+   {
+    frontier_setErrorMsg(__FILE__, __LINE__, "Unmatched parentheses");
+    ret = FRONTIER_ECFG;
+    goto cleanup;
+   }
+
+  ret = FRONTIER_OK;
+
+cleanup:
+  frontier_mem_free(str);
+  return ret;
+ }
+
 int frontierConfig_addServer(FrontierConfig *cfg, const char* server_url)
  {
+  if ((server_url != 0) && (strchr(server_url, '(') != NULL))
+    return frontierConfig_parseComplexServerSpec(cfg, server_url);
+
   if(cfg->server_num >= FRONTIER_MAX_SERVERN) {
-    frontier_log(FRONTIER_LOGLEVEL_DEBUG, __FILE__, __LINE__, 
+    frontier_setErrorMsg(__FILE__, __LINE__, 
       "Reached limit of %d frontier servers", FRONTIER_MAX_SERVERN);    
     return FRONTIER_ECFG;
   }
 
   if(!server_url) {
-    return FRONTIER_ECFG;
+    return FRONTIER_OK;
   }
   else {
     if(!*server_url) {
       frontier_log(FRONTIER_LOGLEVEL_DEBUG, __FILE__, __LINE__, "Empty server url.");    
-      return FRONTIER_ECFG;
+      return FRONTIER_OK;
     }
   }
 
@@ -138,8 +267,8 @@ int frontierConfig_addServer(FrontierConfig *cfg, const char* server_url)
   int i;
   for(i = 0; i < cfg->server_num; ++i) {
     if(strcmp(cfg->server[i], server_url) == 0) {
-      frontier_log(FRONTIER_LOGLEVEL_DEBUG, __FILE__, __LINE__, "Duplicate server url: %s", server_url);    
-      return FRONTIER_ECFG;
+      frontier_log(FRONTIER_LOGLEVEL_DEBUG, __FILE__, __LINE__, "Duplicate server url skipped: %s", server_url);    
+      return FRONTIER_OK;
     }
   }
   
@@ -154,18 +283,18 @@ int frontierConfig_addServer(FrontierConfig *cfg, const char* server_url)
 int frontierConfig_addProxy(FrontierConfig *cfg, const char* proxy_url)
  {
   if(cfg->proxy_num >= FRONTIER_MAX_PROXYN) {
-    frontier_log(FRONTIER_LOGLEVEL_DEBUG, __FILE__, __LINE__, 
+    frontier_setErrorMsg(__FILE__, __LINE__, 
       "Reached limit of %d frontier proxies", FRONTIER_MAX_PROXYN);    
     return FRONTIER_ECFG;
   }
 
   if(!proxy_url) {
-    return FRONTIER_ECFG;
+    return FRONTIER_OK;
   }
   else {
     if(!*proxy_url) {
       frontier_log(FRONTIER_LOGLEVEL_DEBUG, __FILE__, __LINE__, "Empty proxy url.");    
-      return FRONTIER_ECFG;
+      return FRONTIER_OK;
     }
   }
 
@@ -173,8 +302,8 @@ int frontierConfig_addProxy(FrontierConfig *cfg, const char* proxy_url)
   int i;
   for(i = 0; i < cfg->proxy_num; ++i) {
     if(strcmp(cfg->proxy[i], proxy_url) == 0) {
-      frontier_log(FRONTIER_LOGLEVEL_DEBUG, __FILE__, __LINE__, "Duplicate server url: %s", proxy_url);    
-      return FRONTIER_ECFG;
+      frontier_log(FRONTIER_LOGLEVEL_DEBUG, __FILE__, __LINE__, "Duplicate server url skipped: %s", proxy_url);    
+      return FRONTIER_OK;
     }
   }
 
