712f24ed32fb4bb61de711b73abe381427d8f2dc
dwd <dwd>
2006-07-26 18:25:13 +0000

    Add ServeletOutputStream parameter to fdo_get and fdo_write, and pass it on to dbm.acquire and dbm.release.  Reduce debugging output a little.

--- src/gov/fnal/frontier/PluginDataObject.java
+++ src/gov/fnal/frontier/PluginDataObject.java
@@ -5,7 +5,7 @@ import gov.fnal.frontier.fdo.*;
 import gov.fnal.frontier.plugin.*;
 import java.util.*;
 import java.sql.*;
-
+import javax.servlet.ServletOutputStream;
 
 
 public class PluginDataObject implements FrontierDataObject
@@ -22,7 +22,7 @@ public class PluginDataObject implements FrontierDataObject
    {
     if(builtin_name.equals("frontier_request"))
      {
-      System.out.println("PluginDataObject(builtin:frontier_request)");
+      //System.out.println("PluginDataObject(builtin:frontier_request)");
       this.dbm=dbm;
       obj_name=builtin_name;
       obj_version=builtin_version;
@@ -35,7 +35,7 @@ public class PluginDataObject implements FrontierDataObject
   
   protected PluginDataObject(DbConnectionMgr dbm,String requested_name,String requested_version) throws Exception
    {
-    System.out.println("PluginDataObject()");
+    //System.out.println("PluginDataObject()");
     this.dbm=dbm;
     obj_name=requested_name;
     obj_version=requested_version;
@@ -58,21 +58,22 @@ public class PluginDataObject implements FrontierDataObject
    }
      
    
-  public int fdo_get(Encoder enc,String method,FrontierDataStream fds) throws Exception
+  public int fdo_get(Encoder enc,String method,FrontierDataStream fds,ServletOutputStream sos) throws Exception
    {
-    System.out.println("PluginDataObject.fdo_get()");
+    //System.out.println("PluginDataObject.fdo_get()");
     int rec_num=0;
     
     Connection con=null;
     
     try
      {
-      con=dbm.acquire();
+      con=dbm.acquire(sos);
       rec_num=plugin.fp_get(con,enc,method,fds);
      }
     finally
      {
-      if(con!=null) try{dbm.release(con);}catch(Exception e){}
+      //System.out.println("fdo_get releasing dbm");
+      if(con!=null) try{dbm.release(con,sos);}catch(Exception e){}
      }
     return rec_num;
    }
@@ -80,7 +81,7 @@ public class PluginDataObject implements FrontierDataObject
    
   public int fdo_meta(Encoder enc,String method) throws Exception
    {    
-    System.out.println("PluginDataObject.fdo_meta()");
+    //System.out.println("PluginDataObject.fdo_meta()");
     int rec_num;
     
     rec_num=plugin.fp_meta(enc,method);
@@ -88,20 +89,20 @@ public class PluginDataObject implements FrontierDataObject
    }
    
    
-  public int fdo_write(Encoder enc,String method,FrontierDataStream fds) throws Exception   
+  public int fdo_write(Encoder enc,String method,FrontierDataStream fds,ServletOutputStream sos) throws Exception   
    {
-    System.out.println("PluginDataObject.fdo_write()");
+    //System.out.println("PluginDataObject.fdo_write()");
     int rec_num=0;
     
     Connection con=null;
     try
      {
-      con=dbm.acquire();
+      con=dbm.acquire(sos);
       rec_num=plugin.fp_write(con,enc,method,fds);
      }
     finally
      {
-      if(con!=null) try{dbm.release(con);}catch(Exception e){}
+      if(con!=null) try{dbm.release(con,sos);}catch(Exception e){}
      }
     return rec_num;
    }
