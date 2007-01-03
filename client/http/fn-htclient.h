530883d345d21a8b823b0939824671e72c984389
dwd <dwd>
2007-01-03 20:12:50 +0000

    Add interface so that connect, read, and write timeouts are parameters

--- client/http/fn-htclient.h
+++ client/http/fn-htclient.h
@@ -21,9 +21,9 @@
 
 int frontier_socket();
 void frontier_socket_close(int s);
-int frontier_connect(int s,const struct sockaddr *serv_addr,socklen_t addrlen);
-int frontier_write(int s,const char *buf, int len);
-int frontier_read(int s, char *buf, int size);
+int frontier_connect(int s,const struct sockaddr *serv_addr,socklen_t addrlen,int timeoutsecs);
+int frontier_write(int s,const char *buf,int len,int timeoutsecs);
+int frontier_read(int s, char *buf,int size,int timeoutsecs);
 
 
 struct s_FrontierUrlInfo
@@ -57,6 +57,9 @@ struct s_FrontierHttpClnt
   char *frontier_id;
   
   int socket;
+  int connect_timeout_secs;
+  int read_timeout_secs;
+  int write_timeout_secs;
     
   int err_code;
   int data_size;
@@ -71,6 +74,9 @@ int frontierHttpClnt_addServer(FrontierHttpClnt *c,const char *url);
 int frontierHttpClnt_addProxy(FrontierHttpClnt *c,const char *url);
 void frontierHttpClnt_setCacheRefreshFlag(FrontierHttpClnt *c,int is_refresh);
 void frontierHttpClnt_setFrontierId(FrontierHttpClnt *c,const char *frontier_id);
+void frontierHttpClnt_setConnectTimeoutSecs(FrontierHttpClnt *c,int timeoutsecs);
+void frontierHttpClnt_setReadTimeoutSecs(FrontierHttpClnt *c,int timeoutsecs);
+void frontierHttpClnt_setWriteTimeoutSecs(FrontierHttpClnt *c,int timeoutsecs);
 int frontierHttpClnt_open(FrontierHttpClnt *c);
 int frontierHttpClnt_get(FrontierHttpClnt *c,const char *url);
 int frontierHttpClnt_post(FrontierHttpClnt *c,const char *url,const char *body);
