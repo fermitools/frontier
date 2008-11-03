1891b489bbdf2b79342716bee91665047ab02a21
dwd <dwd>
2008-11-03 23:01:16 +0000

    Extend fp_get and fp_write definitions to include a FrontierDataStream parameter.  Add fp_cachedLastModified and fp_getLastModified definitions.

--- src/gov/fnal/frontier/plugin/FrontierPlugin.java
+++ src/gov/fnal/frontier/plugin/FrontierPlugin.java
@@ -6,7 +6,9 @@ public interface FrontierPlugin
  {
   public String[] fp_getMethods();
   public MethodDesc fp_getMethodDesc(String name) throws Exception;
-  public int fp_get(java.sql.Connection con,Encoder enc,String method,FrontierDataStream fds) throws Exception;
+  public int fp_get(java.sql.Connection con,Encoder enc,String method) throws Exception;
   public int fp_meta(Encoder enc,String method) throws Exception;
-  public int fp_write(java.sql.Connection con,Encoder enc,String method,FrontierDataStream fds) throws Exception;  
+  public int fp_write(java.sql.Connection con,Encoder enc,String method) throws Exception;  
+  public long fp_cachedLastModified() throws Exception;
+  public long fp_getLastModified(java.sql.Connection con) throws Exception;
  }
