efedb09bffa85ba7d8e040f54dcf4ca0e9af33a9
dwd <dwd>
2009-07-14 15:38:12 +0000

    Initial install

---
+++ src/gov/fnal/frontier/QueryTableSet.java
@@ -0,0 +1,22 @@
+package gov.fnal.frontier;
+
+import java.util.HashSet;
+import java.util.Iterator;
+
+// this class extends HashSet just to make a prettier toString() for printing
+public class QueryTableSet extends HashSet 
+ {
+  public String toString()
+   {
+    Iterator it=iterator();
+    String name=(String)it.next(); // The first table name
+    String str=name.toUpperCase();
+    while (it.hasNext()) 
+     {
+      str+=" and ";
+      name=(String)it.next();
+      str+=name.toUpperCase();
+     }
+    return str.replaceAll("\"","");
+   }
+ }
