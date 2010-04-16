bf1a18873467a240eeac0c1351cbfbf3e549ee56
dwd <dwd>
2010-04-16 19:39:54 +0000

    No longer keep local copy of expat

--- client/expat/compile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-/bin/sh ./libtool  --mode=compile cc -g -O2 -Wall -Wmissing-prototypes -Wstrict-prototypes -DHAVE_EXPAT_CONFIG_H   -I./lib -I. -o lib/xmlparse.lo -c lib/xmlparse.c
-/bin/sh ./libtool  --mode=compile cc -g -O2 -Wall -Wmissing-prototypes -Wstrict-prototypes -DHAVE_EXPAT_CONFIG_H   -I./lib -I. -o lib/xmltok.lo -c lib/xmltok.c
-/bin/sh ./libtool  --mode=compile cc -g -O2 -Wall -Wmissing-prototypes -Wstrict-prototypes -DHAVE_EXPAT_CONFIG_H   -I./lib -I. -o lib/xmlrole.lo -c lib/xmlrole.c
-
-/bin/sh ./libtool --dry-run  --mode=link cc -g -O2 -Wall -Wmissing-prototypes -Wstrict-prototypes -DHAVE_EXPAT_CONFIG_H   -I./lib -I. -no-undefined -version-info 5:0:5 -rpath /home/serge/work/prog/cvs/frontier/client/expat/../local/lib  -o libexpat.la lib/xmlparse.lo lib/xmltok.lo lib/xmlrole.lo
-
-
