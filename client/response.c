71f4ae8c6d4592f01d86f19b3056729a2202daa2
dwd <dwd>
2013-06-25 20:37:27 +0000

    Add verifying of digital signatures on the response if signatures were requested.

--- client/response.c
+++ client/response.c
@@ -18,6 +18,9 @@
 #include <frontier_client/frontier.h>
 #include "fn-internal.h"
 #include "expat.h"
+#include <openssl/ssl.h>
+#include <openssl/rsa.h>
+#include <openssl/err.h>
 
 extern void *(*frontier_mem_alloc)(size_t size);
 extern void (*frontier_mem_free)(void *ptr);
@@ -85,7 +88,7 @@ xml_startElement(void *userData,const char *name,const char **atts)
 	continue;
        }      
      }
-    fr->payload[fr->payload_num-1]=frontierPayload_create(encoding);
+    fr->payload[fr->payload_num-1]=frontierPayload_create(encoding,(fr->srv_rsakey!=0),fr->params1,fr->params2);
     fr->p_state=FNTR_WITHIN_PAYLOAD;
     return;
    }
@@ -129,6 +132,17 @@ xml_startElement(void *userData,const char *name,const char **atts)
 	fr->payload[fr->payload_num-1]->srv_md5_str[32]=0;
 	continue;       
        }
+      if(strcmp(atts[i],"sig")==0)
+       {
+	int len=strlen(atts[i+1]);
+	unsigned char *srv_sig=frontier_mem_alloc(len*3/4);
+	if(srv_sig)
+	 {
+	  fr->payload[fr->payload_num-1]->srv_sig_len=
+	    fn_base64_ascii2bin((unsigned char *)atts[i+1],len,srv_sig,len*3/4);
+	  fr->payload[fr->payload_num-1]->srv_sig=srv_sig;
+	 }
+       }
      }
    }
  }
@@ -159,7 +173,7 @@ xml_endElement(void *userData,const char *name)
 
 
 
-FrontierResponse *frontierResponse_create(int *ec)
+FrontierResponse *frontierResponse_create(int *ec,void *srv_rsakey,const char *params1,const char *params2)
  {
   FrontierResponse *fr;
   int i;
@@ -168,7 +182,7 @@ FrontierResponse *frontierResponse_create(int *ec)
   if(!fr) 
    {
     *ec=FRONTIER_EMEM;
-    frontier_setErrorMsg(__FILE__,__LINE__,"No more memory");
+    FRONTIER_MSG(*ec);
     return fr;
    }
 
@@ -176,6 +190,9 @@ FrontierResponse *frontierResponse_create(int *ec)
   fr->payload_num=0;
   fr->error_payload_ind=-1;
   fr->keepalives=0;
+  fr->srv_rsakey=srv_rsakey;
+  fr->params1=params1;
+  fr->params2=params2;
 
   fr->parser=XML_ParserCreate(NULL);
   if(!fr->parser)
@@ -238,10 +255,11 @@ int FrontierResponse_append(FrontierResponse *fr,char *buf,int len)
  }
 
 
-
 int frontierResponse_finalize(FrontierResponse *fr)
  {
   int i;
+  unsigned char *digest;
+
   if(XML_Parse(fr->parser,"",0,1)==XML_STATUS_ERROR) 
    {
     int xml_err=XML_GetErrorCode(fr->parser);
@@ -255,7 +273,7 @@ int frontierResponse_finalize(FrontierResponse *fr)
   
   for(i=0;i<fr->payload_num;i++)
    {
-    //printf("%d r:[%s] l:[%s]\n",i,fr->payload[i]->srv_md5_str,fr->payload[i]->md5_str);
+    //printf("%d r:[%s] l:[%s]\n",i,fr->payload[i]->srv_md5_str);
     frontier_log(FRONTIER_LOGLEVEL_DEBUG,__FILE__,__LINE__,"Payload[%d] error %d error code %d",(i+1),fr->payload[i]->error,fr->payload[i]->error_code);
     if(fr->payload[i]->error) 
      {
@@ -267,19 +285,88 @@ int frontierResponse_finalize(FrontierResponse *fr)
       frontier_setErrorMsg(__FILE__,__LINE__,"Payload[%d] has error code %d",(i+1),fr->payload[i]->error_code);
       return FRONTIER_EPROTO;
      }
-    if(strncmp(fr->payload[i]->srv_md5_str,fr->payload[i]->md5_str,32)) 
+
+    digest=fr->payload[i]->digest;
+
+    if(fr->srv_rsakey!=0)
      {
-      frontier_setErrorMsg(__FILE__,__LINE__,"Payload[%d]: MD5 hash mismatch: server [%s], local [%s]",(i+1),fr->payload[i]->srv_md5_str,fr->payload[i]->md5_str);
-      return FRONTIER_EPROTO;
+      unsigned char *sha256_buf=0;
+      int ret=FRONTIER_OK;
+      int siglen=fr->payload[i]->srv_sig_len;
+      int declen;
+
+      if(siglen==0)
+       {
+	frontier_setErrorMsg(__FILE__,__LINE__,"Payload[%d]: no signature found",i+1);
+	return FRONTIER_EPROTO;
+       }
+
+      // If rsa decrypt fails, the output can be as big as the input.
+      // If it succeeds, the length should be SHA256_DIGEST_LENGTH.
+      // So malloc a buffer of the bigger size just in case
+      sha256_buf=frontier_mem_alloc(siglen);
+      if(!sha256_buf) {ret=FRONTIER_EMEM;FRONTIER_MSG(ret);return ret;};
+      declen=RSA_public_decrypt(siglen,fr->payload[i]->srv_sig,sha256_buf,
+      			(RSA *)fr->srv_rsakey,RSA_PKCS1_PADDING);
+      if(declen==-1)
+       {
+	SSL_load_error_strings();
+	frontier_setErrorMsg(__FILE__,__LINE__,"Payload[%d]: error decrypting signature: %s",i+1,ERR_error_string(ERR_get_error(),0));
+	ERR_free_strings();
+	ret=FRONTIER_EPROTO;
+	goto endsigned;
+       }
+      if(declen!=SHA256_DIGEST_LENGTH)
+       {
+	frontier_setErrorMsg(__FILE__,__LINE__,"Payload[%d]: decrypted signature wrong length; expect %d, got %d",i+1,SHA256_DIGEST_LENGTH,declen);
+	ret=FRONTIER_EPROTO;
+	goto endsigned;
+       }
+
+      if(memcmp(digest,sha256_buf,declen)==0)
+        frontier_log(FRONTIER_LOGLEVEL_DEBUG,__FILE__,__LINE__,"Payload[%d] signature passed");
+      else
+       {
+        char digest_str[SHA256_DIGEST_LENGTH*2+1];
+	char sha256_str[SHA256_DIGEST_LENGTH*2+1];
+        for(i=0;i<SHA256_DIGEST_LENGTH;i++)
+	 {
+	  snprintf(&digest_str[i*2],3,"%02x",digest[i]);
+	  snprintf(&sha256_str[i*2],3,"%02x",sha256_buf[i]);
+	 }
+	if(strcmp(digest_str,sha256_str)!=0) 
+	 {
+	  frontier_setErrorMsg(__FILE__,__LINE__,"Payload[%d]: signature hash mismatch: server [%s], local [%s]",(i+1),sha256_str,digest_str);
+	  ret=FRONTIER_EPROTO;
+	  goto endsigned;
+	 }
+       }
+endsigned:
+      if(sha256_buf)frontier_mem_free(sha256_buf);
+      if(ret!=FRONTIER_OK)return ret;
+     }
+    else
+     {
+      char md5_str[sizeof(fr->payload[0]->srv_md5_str)];
+      char *srv_md5_str;
+
+      // this variable is set here to avoid a compiler bug; moving this to
+      //  just before the strncmp below (or eliminating the variable) loses
+      // the upper bits & causes a seg fault, at least on gcc 4.1.2
+      srv_md5_str=fr->payload[i]->srv_md5_str;
+
+      bzero(md5_str,sizeof(md5_str));
+      // convert the binary md5 characters into printable
+      for(i=0;i<16;i++)
+	snprintf(&md5_str[i*2],3,"%02x",digest[i]);
+
+      if(strncmp(srv_md5_str,md5_str,32)!=0) 
+       {
+	frontier_setErrorMsg(__FILE__,__LINE__,"Payload[%d]: MD5 hash mismatch: server [%s], local [%s]",(i+1),srv_md5_str,md5_str);
+	return FRONTIER_EPROTO;
+       }
      }
    }
 
   return FRONTIER_OK;
  }
-
-
-
-
-
-
-
