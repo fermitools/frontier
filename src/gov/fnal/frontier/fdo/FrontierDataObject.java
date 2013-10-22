3581b6e753eb6ab6391492b14e97050bdfbeb4d3
dwd <dwd>
2013-10-22 20:52:50 +0000

    Remove fdo_init() interface.  Add if_modified_since to fdo_getLastModified() interface.

--- src/gov/fnal/frontier/fdo/FrontierDataObject.java
+++ src/gov/fnal/frontier/fdo/FrontierDataObject.java
@@ -4,8 +4,6 @@
  *
  * @author Server Kosyakov
  * @version $Id$
- * @see gov.fnal.frontier.XsdDataObject
- * @see gov.fnal.frontier.Xsd2DataObject
  * @see gov.fnal.frontier.PluginDataObject
  *
  * Copyright (c) 2009, FERMI NATIONAL ACCELERATOR LABORATORY
@@ -22,13 +20,12 @@ import javax.servlet.ServletOutputStream;
 
 public interface FrontierDataObject
  {
-  public void fdo_init(byte[] body) throws Exception;
   public MethodDesc fdo_getMethodDesc(String method) throws Exception;
   public void fdo_start(ServletOutputStream os) throws Exception;
   public int fdo_get(Encoder enc,String method,ServletOutputStream os) throws Exception;
   public int fdo_meta(Encoder enc,String method) throws Exception;
   public int fdo_write(Encoder enc,String method,ServletOutputStream os) throws Exception;
   public long fdo_cachedLastModified() throws Exception;
-  public long fdo_getLastModified(ServletOutputStream os) throws Exception;
+  public long fdo_getLastModified(ServletOutputStream os,long if_modified_since) throws Exception;
   public void fdo_close(ServletOutputStream os) throws Exception;
  }
