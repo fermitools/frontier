dcd08613dd7849e2f0e2457b8b634ed87547ccd1
dwd <dwd>
2014-01-02 22:43:31 +0000

    Add -r and -R options for short & forever ttls. Doubly URL-encode path characters that are not unreserved. Support zipping, but default FRONTIER_RETRIEVEZIPLEVEL to 0.

--- client/fn-fileget.c
+++ client/fn-fileget.c
@@ -19,37 +19,82 @@
 #include <errno.h>
 #include <sys/types.h>
 #include <fcntl.h>
+#include <ctype.h>
 #include "frontier_client/frontier.h"
 
 void usage()
  {
-  fprintf(stderr,"Usage: fn-fileget [-c connect_string] filepath ...\n");
+  fprintf(stderr,"Usage: fn-fileget [-c connect_string] [-r|-R] filepath ...\n");
+  fprintf(stderr,"  -c connect_string: use given connect_string (default from environment)\n");
+  fprintf(stderr,"  -r: request short time-to-live\n");
+  fprintf(stderr,"  -R: request forever time-to-live\n");
   exit(2);
+ }
+
+char *
+doubleurlencode(char *frombuf)
+ {
+  int c,n;
+  char *tobuf=malloc(strlen(frombuf)*5+1);
+  char *p=tobuf;
+  while((c=*frombuf++)!=0)
+   {
+    if(isalnum(c)||(c=='/')||(c=='-')||(c=='_')||(c=='.'))
+      *p++=c;
+    else
+     {
+      // %25 is the encoding of %
+      *p++='%';
+      *p++='2';
+      *p++='5';
+      n=(c>>4);
+      if(n<10)
+        *p++=n+'0';
+      else
+        *p++=n-10+'a';
+      n=(c&0xf);
+      if(n<10)
+        *p++=n+'0';
+      else
+        *p++=n-10+'a';
+     }
+   }
+  *p='\0';
+  return tobuf;
  }
 
 int main(int argc, char **argv)
  {
   int ec,c,i;
   unsigned long channel;
+  int ttl=2;
+  int ziplevel=0;
+  char zipstr[5];
   FrontierConfig *config;
   char *connectstr="";
 
   while(1)
    {
-    c=getopt(argc,argv,":c:");
+    c=getopt(argc,argv,":c:rR");
     switch(c)
      {
       case 'c':
-       connectstr=optarg;
-       break;
+        connectstr=optarg;
+        break;
+      case 'r':
+        ttl=1;
+        break;
+      case 'R':
+        ttl=3;
+        break;
       case -1:
-       break;
+        break;
       case ':':
-       fprintf(stderr,"Missing argument\n");
-       usage();
+        fprintf(stderr,"Missing argument\n");
+        usage();
       default:
-       fprintf(stderr,"Unrecognized option\n");
-       usage();
+        fprintf(stderr,"Unrecognized option\n");
+        usage();
      }
     if(c==-1)
       break;
@@ -60,6 +105,10 @@ int main(int argc, char **argv)
     fprintf(stderr,"No files requested\n");
     usage();
    }
+
+  /* default zip level to 0, unlike ordinary frontier client */
+  if(getenv("FRONTIER_RETRIEVEZIPLEVEL")==0)
+    putenv("FRONTIER_RETRIEVEZIPLEVEL=0");
 
   if(frontier_init(malloc,free)!=0)
    {
@@ -80,17 +129,34 @@ int main(int argc, char **argv)
     return 2;
    }
 
+  frontier_setTimeToLive(channel,ttl);
+  ziplevel=frontier_getRetrieveZipLevel(channel);
+  if(ziplevel==0)
+    zipstr[0]='\0';
+  else
+   {
+    zipstr[0]='z';
+    zipstr[1]='i';
+    zipstr[2]='p';
+    zipstr[3]='0'+ziplevel;
+    zipstr[4]='\0';
+   }
+
   for(i=optind;i<argc;i++){
-    char uribuf[4096];
+    char *uribuf;
     FrontierRSBlob *frsb;
     int fd;
     int n;
     char *p;
     char *localname;
+    char *encodedargv;
 
-    snprintf(uribuf,sizeof(uribuf)-1,
-    	"Frontier/type=frontier_file:1:DEFAULT&encoding=BLOB&p1=%s",argv[i]);
+    encodedargv=doubleurlencode(argv[i]);
+    uribuf=malloc(strlen(encodedargv)+128);
+    sprintf(uribuf,"Frontier/type=frontier_file:1:DEFAULT&encoding=BLOB%s&p1=%s",zipstr,encodedargv);
+    free(encodedargv);
     ec=frontier_getRawData(channel,uribuf);
+    free(uribuf);
     if(ec!=FRONTIER_OK)
      {
       fprintf(stderr,"Error getting data for %s: %s\n",argv[i],frontier_getErrorMsg());
