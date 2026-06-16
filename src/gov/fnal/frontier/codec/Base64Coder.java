d3ed6b5211444ec63d5d703a396c529e879c2038
Carl Vuosalo <cvuosalo@cern.ch>
2026-06-16 16:57:22 -0500

    Change to simpler formula

--- src/gov/fnal/frontier/codec/Base64Coder.java
+++ src/gov/fnal/frontier/codec/Base64Coder.java
@@ -127,17 +127,17 @@ public static int encode (byte[] in, int ip, int iLen, byte[] out, int op) {
 */
 public static byte[] decode (byte[] in) {
    int iLen = in.length;
-   int padLen = 0;
-   while (iLen > 0 && in[iLen-1] == '=') {
-      iLen--;
-      padLen++;
-   }
+   while (iLen > 0 && in[iLen-1] == '=') iLen--;
+
    // Decoding converts every 4 encoded bytes into 3 bytes. The encoded value was padded
-   // to make its length a multiple of 4.
-   // For some integer n, original iLen must be 4n. After padding stripped, if padLen==1,
-   // then with m=n-1, iLen==(4m+3) bytes. Decoded length is 3m+2.
-   // If padLen==2, then iLen=(4m+2) bytes. Decoded length is 3m+1.
-   int oLen = ((iLen * 3) - padLen) / 4;
+   // to make its length a multiple of 4. Each encoded byte is decoded into a 6-bit word.
+   // For some integer n, original iLen must be 4n.  After any padding is stripped
+   // off, decoding will transform input length to output length as follows, with m=n-1:
+   // 4n -> 3n
+   // 4m+2 -> 3m+1 (in the formula below, 3m+int(6/4) = 3m+1) (4 bits dropped at end)
+   // 4m+3 -> 3m+2 (in the formula below, 3m+int(9/4) = 3m+2) (2 bits dropped at end)
+
+   int oLen = (iLen * 3) / 4;
 
    byte[] out = new byte[oLen];
    int ip = 0;
